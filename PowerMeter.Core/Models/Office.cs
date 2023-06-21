a7d2ffd92e233ff2ca4c4c997ad608c38f8a2c9d
RainDance74 <raindance774@gmail.com>
2023-06-22 00:44:26 +0500

    Add Models+DBContext

---
+++ PowerMeter.Core/Models/Office.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PowerMeter.Core.Models;
+
+[Table("offices")]
+[Index(nameof(RoomNumber), Name = "offices_room_number_key", IsUnique = true)]
+public partial class Office
+{
+    public Office()
+    {
+        EnergyConsumptions = new HashSet<EnergyConsumption>();
+        Payments = new HashSet<Payment>();
+        Users = new HashSet<User>();
+    }
+
+    [Key]
+    [Column("office_id")]
+    public int OfficeId { get; set; }
+    [Column("room_number")]
+    [StringLength(15)]
+    public string RoomNumber { get; set; } = null!;
+    [Column("office_name")]
+    [StringLength(50)]
+    public string OfficeName { get; set; } = null!;
+
+    [InverseProperty(nameof(EnergyConsumption.Office))]
+    public virtual ICollection<EnergyConsumption> EnergyConsumptions { get; set; }
+    [InverseProperty(nameof(Payment.Office))]
+    public virtual ICollection<Payment> Payments { get; set; }
+    [InverseProperty(nameof(User.Office))]
+    public virtual ICollection<User> Users { get; set; }
+}
---
+++ PowerMeter.Core/Models/Payment.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace PowerMeter.Core.Models;
+
+[Table("payments")]
+public partial class Payment
+{
+#nullable enable
+    [Key]
+    [Column("payment_id")]
+    public int PaymentId { get; set; }
+    [Column("office_id")]
+    public int OfficeId { get; set; }
+    [Column("start_time", TypeName = "timestamp without time zone")]
+    public DateTime StartTime { get; set; }
+    [Column("end_time", TypeName = "timestamp without time zone")]
+    public DateTime? EndTime { get; set; }
+    [Column("note")]
+    public string? Note { get; set; }
+    [Column("amount")]
+    [Precision(10, 2)]
+    public decimal Amount { get; set; }
+    [Column("payment_link")]
+    [StringLength(255)]
+    public string? PaymentLink { get; set; }
+    [ForeignKey(nameof(OfficeId))]
+    [InverseProperty("Payments")]
+    public virtual Office Office { get; set; } = null!;
+}
