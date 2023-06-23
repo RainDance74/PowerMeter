86140d7b5c5ccf00f86248a6112c8bf4a8f56cf0
RainDance74 <raindance774@gmail.com>
2023-06-23 20:29:42 +0500

    Add TODO

--- PowerMeter/ViewModels/Menu/Users/Cards/ContentGridViewModel.cs
+++ PowerMeter/ViewModels/Menu/Users/Cards/ContentGridViewModel.cs
@@ -31,6 +31,7 @@ public partial class ContentGridViewModel : ObservableRecipient
 
         // Don't make the program wait until the DBContext will be generated with await
         var db = await Task.Run(() => new Core.Data.PowerMeterContext());
+        // TODO: Add roles checking
         var data = await db.Users.ToListAsync();
         // Add every user in the same time 👇
         await Task.WhenAll(data.Select(item => { Source.Add(item); return Task.CompletedTask; }));
